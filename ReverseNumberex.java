532bae44142506c63d8fcf41270726527ef8f7f9
gayathrisathiyamoorthi <gayathri.s1313029@ksrce.ac.in>
2016-09-02 12:24:11 +0530

    Add files via upload

---
+++ ReverseNumberex.java
@@ -0,0 +1,20 @@
+package com.guvi.first;
+
+import java.util.Scanner;
+
+public class ReverseNumberex {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int n=scanner.nextInt();
+		int a=0,b=0;
+		while(n>0){
+			a=n%10;
+			n=n/10;
+			b=b*10+a;
+		}
+		System.out.println(b);
+
+	}
+
+}
